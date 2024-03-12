a0622b36ce04369b5bce53e24695d8196798ac65
Rabinjais <dunzo901@gmail.com>
2024-03-12 04:41:20 +0000

    developed stg_orders improved

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -5,6 +5,21 @@
     )
 }}
 
-select *, 
-ordersellingprice- ordercostprice as orderprofit
-from {{ ref('raw_orders') }}
+select o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice- o.ordercostprice as orderprofit,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} o
+left join {{ ref('raw_customers') }} c
+on o.customerid = c.customerid
+left join {{ ref('raw_products') }} p
+on o.productid = p.productid
